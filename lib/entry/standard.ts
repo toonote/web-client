d2d9d396234b30503f6a7387d76f60b76c2daa16
TooBug <i@toobug.net>
2019-09-22 15:01:10 +0800

    standard build

--- lib/entry/standard.ts
+++ lib/entry/standard.ts
@@ -9,9 +9,15 @@ import WebClientController from '../controller/main';
 import WebClientModel from '../model/main';
 import view from '../view/main';
 
+declare var process:any;
+
+const BASE_URL = process.env.NODE_ENV === 'production' ?
+            'https://api.xiaotu.io':
+            'https://test-api.xiaotu.io';
+
 const model = new WebClientModel({
     localStorage: 'ls',
-    endpoint: 'https://test-api.xiaotu.io/api/v2/',
+    endpoint: BASE_URL + '/api/v2/',
     headers: {}
 })
 
